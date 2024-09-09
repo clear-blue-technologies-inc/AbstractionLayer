aec8ac116125e2fcb80a8512873713b76e85e75e
Ben Haubrich <benjamin@clearbluetechnologies.com>
2024-09-09 17:03:21 -0400

    Make sure the allocate function always returns the pointer to what it just allocated and add in an assert.

--- Modules/MemoryManagement/Esp/MemoryConfigModule.cpp
+++ Modules/MemoryManagement/Esp/MemoryConfigModule.cpp
@@ -1,20 +1,24 @@
 #include "esp_heap_caps.h"
-
+#include <cassert>
 //Going off the strategies listed here: https://docs.espressif.com/projects/esp-idf/en/v5.1.1/esp32/api-reference/kconfig.html#config-spiram-malloc-alwaysinternal
 //https://docs.espressif.com/projects/esp-idf/en/v5.1.1/esp32/api-reference/system/mem_alloc.html
 static void *allocate(size_t size) {
     constexpr size_t spiramThreshold = 8196;
+    void *ptr = nullptr;
 
     if (size <= spiramThreshold) {
-        if (nullptr == heap_caps_malloc(size, MALLOC_CAP_8BIT)) {
+        if (nullptr == (ptr = heap_caps_malloc(size, MALLOC_CAP_8BIT))) {
             return heap_caps_malloc(size, MALLOC_CAP_SPIRAM);
         }
     }
     else {
-        if (nullptr == heap_caps_malloc(size, MALLOC_CAP_SPIRAM)) {
+        if (nullptr == (ptr = heap_caps_malloc(size, MALLOC_CAP_SPIRAM))) {
             return heap_caps_malloc(size, MALLOC_CAP_8BIT);
         }
     }
+
+    assert(nullptr != ptr);
+    return ptr;
 }
 
 void *operator new(size_t size) {
