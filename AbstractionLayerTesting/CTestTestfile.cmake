a2d684c7ac0b822bf82d8981afcff787bbf128df
Ben Haubrich <benjamin@clearbluetechnologies.com>
2024-09-19 21:27:41 -0400

    Remove some test files or cmake.

--- AbstractionLayerTesting/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/benhaub/AbstractionLayerExample/AbstractionLayer/AbstractionLayerTesting
-# Build directory: /home/benhaub/AbstractionLayerExample/AbstractionLayer/AbstractionLayerTesting
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("Example")
--- AbstractionLayerTesting/Example/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/benhaub/AbstractionLayerExample/AbstractionLayer/AbstractionLayerTesting/Example
-# Build directory: /home/benhaub/AbstractionLayerExample/AbstractionLayer/AbstractionLayerTesting/Example
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(Example "/home/benhaub/AbstractionLayerExample/AbstractionLayer/AbstractionLayerTesting/Example/ExampleTest")
-set_tests_properties(Example PROPERTIES  TIMEOUT "10" _BACKTRACE_TRIPLES "/home/benhaub/AbstractionLayerExample/AbstractionLayer/AbstractionLayerTesting/Example/CMakeLists.txt;53;add_test;/home/benhaub/AbstractionLayerExample/AbstractionLayer/AbstractionLayerTesting/Example/CMakeLists.txt;0;")
