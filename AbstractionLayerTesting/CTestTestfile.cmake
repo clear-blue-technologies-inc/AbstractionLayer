da752b83d7a16f33af2c0b801f668267e7625f3c
Ben Haubrich <benjamin@clearbluetechnologies.com>
2024-09-29 14:24:45 -0400

    FND-115 Untrack files from test.

--- AbstractionLayerTesting/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/benhaubrich/cbtProjects/foundationProject/foundation/main/AbstractionLayer/AbstractionLayerTesting
-# Build directory: /Users/benhaubrich/cbtProjects/foundationProject/foundation/main/AbstractionLayer/AbstractionLayerTesting
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("Example")
-subdirs("OperatingSystem")
-subdirs("Storage")
