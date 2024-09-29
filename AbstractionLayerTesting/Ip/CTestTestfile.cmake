ef51f23cabede2056a0f5b46bbf81d28c20f70cf
Ben Haubrich <benjamin@clearbluetechnologies.com>
2024-09-29 13:50:59 -0400

    FND-115 Get rid of a bunch of tracked test files.

--- AbstractionLayerTesting/Ip/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/benhaubrich/cbtProjects/foundationProject/foundation/main/AbstractionLayer/AbstractionLayerTesting/Ip
-# Build directory: /Users/benhaubrich/cbtProjects/foundationProject/foundation/main/AbstractionLayer/AbstractionLayerTesting/Ip
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(Ip "/Users/benhaubrich/cbtProjects/foundationProject/foundation/main/AbstractionLayer/AbstractionLayerTesting/Ip/cbtIpTest.Mach-O")
-set_tests_properties(Ip PROPERTIES  TIMEOUT "10" _BACKTRACE_TRIPLES "/Users/benhaubrich/cbtProjects/foundationProject/foundation/main/AbstractionLayer/AbstractionLayerTesting/Ip/CMakeLists.txt;73;add_test;/Users/benhaubrich/cbtProjects/foundationProject/foundation/main/AbstractionLayer/AbstractionLayerTesting/Ip/CMakeLists.txt;0;")
--- AbstractionLayerTesting/OperatingSystem/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/benhaubrich/cbtProjects/foundationProject/foundation/main/AbstractionLayer/AbstractionLayerTesting/OperatingSystem
-# Build directory: /Users/benhaubrich/cbtProjects/foundationProject/foundation/main/AbstractionLayer/AbstractionLayerTesting/OperatingSystem
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(OperatingSystem "/Users/benhaubrich/cbtProjects/foundationProject/foundation/main/AbstractionLayer/AbstractionLayerTesting/OperatingSystem/OperatingSystemTest")
-set_tests_properties(OperatingSystem PROPERTIES  TIMEOUT "10" _BACKTRACE_TRIPLES "/Users/benhaubrich/cbtProjects/foundationProject/foundation/main/AbstractionLayer/AbstractionLayerTesting/OperatingSystem/CMakeLists.txt;58;add_test;/Users/benhaubrich/cbtProjects/foundationProject/foundation/main/AbstractionLayer/AbstractionLayerTesting/OperatingSystem/CMakeLists.txt;0;")
